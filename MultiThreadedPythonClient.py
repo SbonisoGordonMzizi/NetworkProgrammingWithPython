86997013da86fb4022011329f400d1905583a427
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-02-24 21:16:35 +0200

    MultiThreadedServer

---
+++ MultiThreadedPythonClient.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+  Python base client server
+  can recieve and process any object
+
+"""
+
+import socket
+import pickle
+
+HEADERSIZE = 10
+PORT=8081
+
+def sendMessageToServer(clientSocketObject ,clientQuery):
+    message = clientQuery.lower()
+    messageWithMessageLength = f"{len(message):<10}"+message
+    clientSocketObject.send(bytes(messageWithMessageLength,"utf-8"))
+
+
+def userInput(clientSocketObject):
+    while True:
+        clientQuery = input(">>  ")
+        if clientQuery.lower() == "exit":
+            sendMessageToServer(clientSocketObject ,clientQuery)
+            return 0
+            break
+
+        elif clientQuery.lower() == "list":          
+            sendMessageToServer(clientSocketObject ,clientQuery)
+            message = clientSocketObject.recv(8192)
+            print(message.decode("utf-8"))
+
+        elif clientQuery.lower() == "get":
+            sendMessageToServer(clientSocketObject ,clientQuery)
+            message = clientSocketObject.recv(8192)
+            print(message.decode("utf-8"))
+
+        else:
+            print(
+                """
+                Enter exit     : to quite the application
+                Enter list     : to list files
+                Enter get      : to download file
+                """)
+             
+def clientMain():
+    clientSocketObject = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    clientSocketObject.connect((socket.gethostname(),PORT))
+    while True:
+        full_message = ''
+        new_massage = True
+        while True:
+            message = clientSocketObject.recv(8192)
+            if new_massage:
+                transmittedDatalength = int(message[:10])
+                new_massage = False
+
+            full_message += message.decode("utf-8")    
+        
+            if len(full_message) - HEADERSIZE == transmittedDatalength:
+                print(full_message[10:])
+                full_message = ''
+                new_massage = True
+                hold = userInput(clientSocketObject)
+                if hold == 0:
+                    break
+        clientSocketObject.close()
+        break
+    
+if __name__ == '__main__':
+
+    clientMain()
+
+       
+    
+    
+    
---
+++ MultiThreadedPythonServer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+ Python base Server
+ This Server is a sigle Threaded and it can backlog the maximum of 200 client request
+ Transmit any Object
+"""
+
+import socket
+import pickle #module used to convert objects to bytes
+from threading import Thread
+import time
+
+HEADERSIZE = 10 
+SERVER_IPADD = socket.gethostname()
+PORT = 8081
+
+def sendMessageToClient(clientRequestObject,message):
+    messageWithMessageLength = f"{len(message):<10}"+message
+    clientRequestObject.send(bytes(messageWithMessageLength,"utf-8"))
+    
+
+def requestHandler(clientRequestObject,ipAdderss):
+    print(f"client connected {ipAdderss}")
+    message = "\nWELCOME TO FILE SERVER"
+    sendMessageToClient(clientRequestObject,message)
+    while True:
+        message = clientRequestObject.recv(8192)
+        clientMessage = message.decode("utf-8")
+        
+        try:
+            if clientMessage[10:] == "list":
+                list_message = "....listing all files in a server...."
+                sendMessageToClient(clientRequestObject,list_message)
+                
+            elif clientMessage[10:] == "get":
+                get_message = "...downloading file from server..."
+                sendMessageToClient(clientRequestObject,get_message)
+            
+            elif clientMessage[10:] == "exit":
+                clientRequestObject.close()
+                break
+       
+            else:
+                message = "...Command Unknown..."
+                sendMessageToClient(clientRequestObject,message)
+        except  BrokenPipeError:
+            pass 
+   
+        
+def main():
+    print("[SERVER STATING ...]")
+    serverSockerObject = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    serverSockerObject.bind((socket.gethostname(),PORT))
+    serverSockerObject.listen(1000) 
+    print(f"[SERVER LISTENING ON ({SERVER_IPADD}:{PORT})]")
+    count = 0
+    while True:
+        clientRequestObject,ipAdderss = serverSockerObject.accept()
+        print("clientObject Created")
+        clientthread = Thread(target=requestHandler,args=(clientRequestObject,ipAdderss))
+        clientthread.start()
+        print("Name : ",clientthread.getName())
+    
+
+if __name__ == '__main__':
+
+    main()
